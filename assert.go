0c0753ffc05dc3bba0373bc4b10020b0c52609fa
namphamtoday <nampham.today@gmail.com>
2022-12-25 18:42:04 +0700

    add helper

--- assert.go
+++ assert.go
@@ -3,6 +3,7 @@ package assert
 import "testing"
 
 func AssertEqual[T comparable](t *testing.T, expect T, output T) {
+	t.Helper()
 	if expect != output {
 		t.Errorf("========> FAILED: Expect %v - Ouput %v", expect, output)
 	} else {
